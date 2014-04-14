138bdcd0a8c22e6f4aa6c1792feccd84826f5351
Yevhen Maryevych <yevhen.maryeyvch@gmail.com>
2014-04-14 09:27:55 +0300

    v 0.0.1 dev

--- app/src/main/java/ua/pp/a_i/push/app/MainActivity.java
+++ app/src/main/java/ua/pp/a_i/push/app/MainActivity.java
@@ -3,6 +3,8 @@ package ua.pp.a_i.push.app;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,49 +20,53 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class MainActivity extends Activity {
 
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    static final String TAG="Push";
+    private static final String PROPERTY_REG_ID="registration_id";
+    private static final String PROPERTY_APP_VERSION="0.0.1 dev";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST=9000;
+    String SENDER_ID="862066542340";
+    private final static String TAG="PushApp";
+
     GoogleCloudMessaging gcm;
+    AtomicInteger msgId=new AtomicInteger();
+    SharedPreferences prefs;
     Context context;
-    static String regid;
+    String regId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(checkPlayServices()){
-            Log.i("wsdfgs","rfgw");
-        }
 
-
-        if(regid==null){
-            context = getApplicationContext();
+        if (checkPlayServices()) {
             gcm = GoogleCloudMessaging.getInstance(this);
-            new AsyncTask<Void,Void,Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-                    try {
-                        String model=Build.MODEL;
-                        String user=Build.USER;
-                        regid = gcm.register("862066542340");
-                        HttpGet request = new HttpGet("http://a-i.pp.ua/push/regdevice?id=" + regid+"&model="+model+"&user="+user);
-                        HttpClient client = new DefaultHttpClient();
-                        client.execute(request);
-                    } catch (Exception e) {
+            regId = getRegistrationId(context);
+            if (regId.isEmpty()) {
+                registerInBackground();
+            }
 
-                    }
-                    return null;
-                }
-            }.execute();
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
         }
     }
 
-    private Boolean checkPlayServices() {
-        int resultCode= GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if(resultCode!= ConnectionResult.SUCCESS){
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode=GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(resultCode!=ConnectionResult.SUCCESS){
             if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
                 GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
             }
@@ -73,15 +79,71 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("regid",regid);
+    private String getRegistrationId(Context context) {
+        final  SharedPreferences prefs=getGCMPreferences(context);
+        String registrationId=prefs.getString(PROPERTY_REG_ID,"");
+        if(registrationId.isEmpty()){
+            Log.i(TAG,"Registration not found.");
+            return "";
+        }
+        int registeredVersion=prefs.getInt(PROPERTY_APP_VERSION,Integer.MIN_VALUE);
+        int currentVersion=getAppVersion(context);
+        if(registeredVersion!=currentVersion){
+            Log.i(TAG,"App version changed.");
+            return "";
+        }
+        return registrationId;
     }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        regid=savedInstanceState.getString("regid");
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        }
+        catch (PackageManager.NameNotFoundException e){
+            throw new RuntimeException("Could not et package name: "+e);
+        }
     }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        return getSharedPreferences(MainActivity.class.getSimpleName(),Context.MODE_PRIVATE);
+    }
+
+    private void registerInBackground() {
+        new AsyncTask<Void,Void,String>(){
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg="";
+                try{
+                    if(gcm==null){
+                        gcm=GoogleCloudMessaging.getInstance(context);
+                    }
+                    regId=gcm.register(SENDER_ID);
+                    msg="Device registered. Registration ID="+regId;
+                    sendRegistrationIdToBackend();
+                    storeRegistrationId(context,regId);
+                }
+                catch (IOException e){
+                    msg="Error:"+e.getMessage();
+                }
+                return msg;
+            }
+        }.execute();
+    }
+
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs=getGCMPreferences(context);
+        int appVersion=getAppVersion(context);
+        Log.i(TAG,"Saving regId on app version "+appVersion);
+        SharedPreferences.Editor editor=prefs.edit();
+        editor.putString(PROPERTY_REG_ID,regId);
+        editor.putInt(PROPERTY_APP_VERSION,appVersion);
+        editor.commit();
+    }
+
+    private void sendRegistrationIdToBackend() {
+
+    }
+
 }
--- app/src/main/java/ua/pp/a_i/push/app/PushBroadcastReceiver.java
+++ app/src/main/java/ua/pp/a_i/push/app/PushBroadcastReceiver.java
@@ -1,6 +1,8 @@
 package ua.pp.a_i.push.app;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,18 +20,27 @@ public class PushBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        GoogleCloudMessaging gcm=GoogleCloudMessaging.getInstance(context);
-        String type=gcm.getMessageType(intent);
-        Bundle extras=intent.getExtras();
+        ComponentName comp = new ComponentName(context.getPackageName(), PushIntentService.class.getName());
+        startWakefullService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+
+        //
+
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        String type = gcm.getMessageType(intent);
+        Bundle extras = intent.getExtras();
         try {
             JSONObject data = new JSONObject(extras.getString("data"));
-            String message=data.getString("message");
-            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+            String message = data.getString("message");
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            String ex = e.getMessage();
 
         }
-        catch (Exception e){
-            String ex=e.getMessage();
+    }
 
-        }
+    private void startWakefullService(Context context, Intent intent) {
+
     }
 }
